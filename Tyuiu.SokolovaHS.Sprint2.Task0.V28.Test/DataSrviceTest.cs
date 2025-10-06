5adea8989337d4b2d6bb4e49d47eeacff709b559
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-10-06 18:51:45 +0500

    Add project files.

---
+++ Tyuiu.SokolovaHS.Sprint2.Task0.V28.Test/DataSrviceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SokolovaHS.Sprint2.Task0.V28.Lib;
+namespace Tyuiu.SokolovaHS.Sprint2.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataSrviceTest
+    {
+        [TestMethod]
+        public void GetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 111;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, false, false, false, false, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint2.Task0.V28/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaHS.Sprint2.Task0.V28.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint2.Task0.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 111;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнила: Соколова Х. С. | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операций сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Соколова Христина Сергеевна | ПКТБ-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("X = " + x +                                                              "*");
+            Console.WriteLine("Y = " + y +                                                              "*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
