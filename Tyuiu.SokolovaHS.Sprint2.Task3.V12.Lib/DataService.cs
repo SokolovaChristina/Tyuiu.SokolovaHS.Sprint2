fc89f841e1fa24e34d94fa8be230b1ec6aa1e56d
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-10-07 00:54:08 +0500

    Task3

---
+++ Tyuiu.SokolovaHS.Sprint2.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SokolovaHS.Sprint2.Task3.V12.Lib
+{
+    public class DataService : ISprint2Task3V12
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+
+            if (x > 1)
+            {
+                // x + ((x+1)/(x-1))^x
+                y = x + Math.Pow((x + 1) / (x - 1), x);
+            }
+            else if (x == 0)
+            {
+                // 1 + cos(√(x+1))
+                y = Math.Round(1 + Math.Cos(Math.Sqrt(x + 1)), 3);
+            }
+            else if ((-16 < x) && (x < 2))
+            {
+                // (7 + 5/x²)^x
+                y = Math.Pow(7 + (5 / (x * x)), x);
+            }
+            else if (x < -16)
+            {
+                // x + 10x - (1/x)
+                y = x + (10 * x) - (1 / x);
+            }
+
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint2.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.SokolovaHS.Sprint2.Task3.V12.Lib;
+namespace Tyuiu.SokolovaHS.Sprint2.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 11;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1.540;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 12;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -18;
+            double res = ds.Calculate(x);
+            double wait = -197.944;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
