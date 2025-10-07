e2d79e08e77dbcc19e1559f2a15a31dc581abd8f
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-10-07 11:40:26 +0500

    Task4

---
+++ Tyuiu.SokolovaHS.Sprint2.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.SokolovaHS.Sprint2.Task4.V5.Lib;
+namespace Tyuiu.SokolovaHS.Sprint2.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidConditution1()
+        {
+            DataService ds = new DataService();
+            double x = 8;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 59.537;
+            Assert.Equals(wait, res);
+
+
+        }
+        [TestMethod]
+        public void ValidConditution2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 25;
+            Assert.Equals(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint2.Task4.V5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaHS.Sprint2.Task4.V5.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint2.Task4.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Соколова Х. С. | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Соколова Христина Сергеевна | ПКТБ-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* c использованием тернарного оператора                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x,y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функций = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
